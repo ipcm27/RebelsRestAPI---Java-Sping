98be07ad4e274e247bbf7090d2530ef55e341d57
Bentroen <bemcdc@gmail.com>
2022-03-07 21:34:35 -0300

    Adicionar arquivos iniciais

---
+++ src/main/java/br/com/letscode/rebels/RebelsRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.letscode.rebels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebelsRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RebelsRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/letscode/rebels/RebelsRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.letscode.rebels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebelsRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
