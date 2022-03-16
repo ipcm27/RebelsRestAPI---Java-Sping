61298714ece8fce386b7040324d31714e0705d0d
Bentroen <bemcdc@gmail.com>
2022-03-16 00:18:36 -0300

    Criar métodos get e save no RebeldeRepository

--- src/main/java/br/com/letscode/rebels/repository/RebeldeRepository.java
+++ src/main/java/br/com/letscode/rebels/repository/RebeldeRepository.java
@@ -1,6 +1,43 @@
-package StarWarsRestAPIJavaSpring.letscode.repository;
+package br.com.letscode.rebels.repository;
 
-public interface RebeldeRepository {
+import br.com.letscode.rebels.model.RebeldeEntity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RebeldeRepository {
+
+    private static List<RebeldeEntity> rebeldes = new ArrayList<RebeldeEntity>();
+
+    // TODO(Bernardo): Rebeldes temporários para teste
+    static {
+        rebeldes.addAll(Arrays.asList(
+                RebeldeEntity.builder()
+                        .nome("Rebelde1")
+                        .idade(42)
+                        .genero('M')
+                        .lat(21.334)
+                        .lon(63.846)
+                        .nomeBase("Echo")
+                        .build(),
+                RebeldeEntity.builder()
+                        .nome("Rebelde2")
+                        .idade(38)
+                        .genero('F')
+                        .lat(24.473)
+                        .lon(54.972)
+                        .nomeBase("Echo 3-T-8")
+                        .build()
+        ));
+    }
+
+    public static List<RebeldeEntity> getAll() {
+        return rebeldes;
+    }
+
+    public static void save(RebeldeEntity entity) {
+        rebeldes.add(entity);
+    }
 
 }
