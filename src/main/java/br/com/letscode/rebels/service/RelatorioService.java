4359854b6a5963e364ac281d79084531cf30c9f2
Igor Pierre <62574338+ipcm27@users.noreply.github.com>
2022-03-16 21:02:03 -0300

    adicionando alguns metodos

--- src/main/java/br/com/letscode/rebels/service/RelatorioService.java
+++ src/main/java/br/com/letscode/rebels/service/RelatorioService.java
@@ -1,4 +1,12 @@
 package br.com.letscode.rebels.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class RelatorioService {
+	
+	 @Autowired
+	    private RebeldeService rebeldeService;
+
+public RelatorioTraidores percentualDeTraidores() {}
+
 }
